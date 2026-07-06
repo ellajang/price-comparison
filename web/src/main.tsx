9fa425aaafe7f5b3e26bf61d630ed535d8270eba
JungEun <108779244+ellajang@users.noreply.github.com>
2026-07-06 11:29:20 +0900

    chore: 가격 갱신

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,7 +5,7 @@ import '@/styles.css';
 
 const rootEl = document.getElementById('root');
 if (!rootEl) {
-  throw new Error('#root 엘리먼트를 찾을 수 없습니다');
+  throw new Error('#root 엘리먼트를 찾을 수 없습니다.');
 }
 
 createRoot(rootEl).render(
